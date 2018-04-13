701a63cb77440d3f67abed37657e94e4aa9e0afb
Sean Febo <seanfebo@techie.com>
2018-04-13 05:25:43 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+  attr_accessor :name, :posts 
+  @@post_count = 0 
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.author = self 
+    @@post_count += 1 
+  end
+  
+  def add_post_by_name(name)
+    post = Post.new(name)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+  
+  def self.post_count
+    @@post_count
+  end
+    
+end
