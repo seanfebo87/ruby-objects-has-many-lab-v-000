6be793ad6ba4334eb4a0dd3ba13c6ad4ac5b4f44
Sean Febo <seanfebo@techie.com>
2018-04-13 05:42:33 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+  attr_accessor :author, :title
+  def initialize(title)
+    @title = title
+    @author = author
+  end
+  
+  def author_name
+   if self.author
+     self.author.name 
+    else
+      nil
+  end
+end
+    
+end
